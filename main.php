82e6f63785bea61926ae2751044768a86aee1a15
Lujillebanal <lujillebanal08@gmail.com>
2025-08-13 02:28:30 +0000

    UnderstandingTheFunctions

--- main.php
+++ main.php
@@ -7,12 +7,17 @@
             return $a + $b;
         case 'subtract':
             return $a - $b;
+        case 'product':
+            return $a * $b;
+        case 'quotient':
+            return $a / $b;
         default:
         return "Invalid Operation";
       }
     }
 
-    echo calculate(a: 10 , b:20, command: 'add');
-    echo "\n";
-     echo calculate(a: 50 , b:70, command: 'subtract'); 
+    echo calculate(a: 10 , b:5, command: 'add') . "<br>";
+    echo calculate(a: 20 , b:10, command: 'subtract') . "<br>"; 
+    echo calculate(a: 5 , b:7, command: 'product') .  "<br>";
+    echo calculate(a: 7 , b:2, command: 'quotient') .  "<br>";
 ?>
